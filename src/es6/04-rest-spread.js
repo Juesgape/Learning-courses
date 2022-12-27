ca838fd94d8fcebdc6132af02fb6f709a87c1608
unknown <juesgape11@gmail.com>
2022-12-26 23:41:03 -0500

    rest-spread

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,13 @@
+// arrays destructuring 
+
+let fruits = ['Apple', 'Banana']
+let [a, b] = fruits
+
+console.log(a, b)
+
+// Object destructuring
+
+let user = {username: 'Juesgape', age: 17};
+let { username, age } = user
+
+console.log(username, age)
