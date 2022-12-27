6aba815e3a297fd687bdaf3649373ec90016adfe
unknown <juesgape11@gmail.com>
2022-12-26 23:07:03 -0500

    Template literals and Multi-line Strings

---
+++ src/es6/02-strings.js
@@ -0,0 +1,13 @@
+let hello = 'Hello'
+let world = 'World'
+//Template literals
+let epicPhrase = `${hello} ${world}!`
+
+console.log(epicPhrase)
+
+// Multi-line Strings
+
+let lorem = `This is an epic phrase
+and this is its continuation`;
+
+console.log(lorem)
