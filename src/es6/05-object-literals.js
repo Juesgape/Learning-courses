7f180892551434c7a5772a41b47ef97feb0a6c07
unknown <juesgape11@gmail.com>
2022-12-28 11:11:27 -0500

    enhanced object literals

---
+++ src/es6/05-object-literals.js
@@ -0,0 +1,12 @@
+// enhanced  object literals
+
+function newUser(user, age, country, uId) {
+    return {
+        user,
+        age,
+        country,
+        id,
+    }
+}
+
+console.log(newUser('Juesgape', 22, 'COL', 1))
