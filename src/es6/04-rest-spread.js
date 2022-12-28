321a988cf08df64f537d5db87226207ee81446ea
unknown <juesgape11@gmail.com>
2022-12-28 11:01:08 -0500

    spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -11,3 +11,22 @@ let user = {username: 'Juesgape', age: 17};
 let { username, age } = user
 
 console.log(username, age)
+
+// spread operator 
+
+let person = {name: 'Juan', age: 18};
+let country = 'COL'
+
+let data = {id: 1, ...person, country}
+
+console.log(data);
+
+// rest
+
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 1)
