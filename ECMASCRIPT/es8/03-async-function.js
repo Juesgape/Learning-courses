c4dc4cf20ed62511cba2157d3849846234fa22b3
unknown <juesgape11@gmail.com>
2022-12-31 11:02:50 -0500

    forcing an error

--- ECMASCRIPT/es8/03-async-function.js
+++ ECMASCRIPT/es8/03-async-function.js
@@ -14,6 +14,7 @@ const data = [{
 
 const getData = () => {
     return new Promise((resolve, reject) => {
+        //Forcing an error
         if(data.length === 0) {
             setTimeout(() => {
                 resolve(data)
