3fa40d3d552fa3cd4f0432a329ba89c3775eea60
unknown <juesgape11@gmail.com>
2023-01-03 17:39:35 -0500

    optional chaining

--- ECMASCRIPT/es9/02-trim.js
+++
@@ -1,4 +0,0 @@
-const saludo = "      hola      "
-console.log(saludo.trim())
-console.log(saludo.trimStart());
-console.log(saludo.trimEnd());
--- ECMASCRIPT/es9/fromentries.js
+++
@@ -1,3 +0,0 @@
-const entries = new Map([['name', 'Juan'], ['age', 34]]);
-console.log(entries);
-console.log(Object.fromEntries(entries));
