179ab429b2461ef3a59f2ae8af495563426e367f
unknown <juesgape11@gmail.com>
2023-01-03 18:09:35 -0500

    Promise allSettled

---
+++ ECMASCRIPT/es11/03-promise-allset.js
@@ -0,0 +1,7 @@
+const promise1 = new Promise((resolve, reject) => reject('Reject'))
+const promimse2 = new Promise((resolve, reject) => resolve('Resolve'))
+
+const promise3 = new Promise((resolve, reject) => resolve('Resolve 2'))
+
+Promise.allSettled([promise1, promimse2, promise3])
+    .then(response => console.log(response));
